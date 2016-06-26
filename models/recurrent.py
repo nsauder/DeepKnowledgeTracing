86343b5eb54b5607a73cd27b1ca9297373edfac6
Nate Sauder <nathaniel.sauder@gmail.com>
2016-06-26 00:41:06 -0400

    Cleaned up RNN file

--- models/recurrent.py
+++ models/recurrent.py
@@ -33,27 +33,3 @@ class RNN(Recurrent):
                                                 (self.num_output,))
 
         
-
-
-class LSTM(Recurrent):
-    def __init__(self, *args):
-        super(RNN, self).__init__(*args)
-        self.w_hh = utils.create_shared_variable(lasagne.init.Orthogonal,
-                                           (self.num_hidden, self.num_hidden))
-        self.w_xh = utils.create_shared_variable(lasagne.init.Orthogonal,
-                                           (self.num_input, self.num_hidden))
-        self.w_hy = utils.create_shared_variable(lasagne.init.Orthogonal,
-                                           (self.num_hidden,self.num_output))
-        self.h = utils.create_shared_variable(lasagne.init.Uniform,
-                                        (self.num_hidden,))
-        self.b_h = utils.create_shared_variable(lasagne.init.Uniform,
-                                          (self.num_hidden,))
-        self.b_y = utils.create_shared_variable(lasagne.init.Uniform,
-                                          (self.num_output,))
-
-        
-    def step(self, x, h):
-        new_h = T.nnet.sigmoid(T.dot(h, self.w_hh) + T.dot(x, self.w_xh) + b_h)
-        new_y = T.nnet.softmax(T.dot(new_h, self.w_hy) + b_y)
-
-        return new_h, new_y
